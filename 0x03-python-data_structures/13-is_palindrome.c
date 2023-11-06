5238d1fceecff8f0d180435f60423fd11610991f
Mubby2003 <adetonamubarak12@gmail.com>
2023-11-06 15:00:55 -0800

    “code6”

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,15 +8,10 @@
  */
 void reverse_listint(listint_t **head)
 {
-  listint_t *prev = NULL;
   listint_t *current = *head;
-  listint_t *next = NULL;
 
   while (current)
     {
-      next = current->next;
-      current->next = prev;
-      prev = current;
       current = next;
     }
 
