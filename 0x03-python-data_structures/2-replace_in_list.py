f67d041714132dd81aa6dfc6df2fc0e8c0938414
Mubby2003 <adetonamubarak12@gmail.com>
2023-11-06 09:13:41 -0800

    “code3”

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,12 +1,6 @@
+
 #!/usr/bin/python3
-
 def replace_in_list(my_list, idx, element):
-    num_element = len(my_list)
-
-    if idx < 0:
-        return my_list
-    if idx >= num_element:
-        return my_list
-
-    my_list[idx] = element
-    return my_list)
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
