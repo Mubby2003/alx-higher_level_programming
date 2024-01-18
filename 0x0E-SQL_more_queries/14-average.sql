1afe11a225ac8ca29a86847e58c1d0a5bbdcf18f
Mubby2003 <adetonamubarak12@gmail.com>
2024-01-17 19:07:10 -0800

    good

--- 0x0E-SQL_more_queries/14-average.sql
+++
@@ -1,2 +0,0 @@
--- A script that computes the average score of all records in the table second_table.
-SELECT AVG(score) AS "average" FROM second_table;
--- 0x0E-SQL_more_queries/16-no_link.sql
+++
@@ -1,6 +0,0 @@
--- A script that lists all records of the table second_table having a name value.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC;
